a1ef68a84420bdba197690e156c9e29b7d9ba057
Augusto Eduardo <augustoedt@gmail.com>
2023-03-13 11:14:41 -0300

    update to pnpm

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config';
-import { logger } from './utils/logger';
+import "dotenv/config";
+import { logger } from "./utils/logger";
 
 logger.info(`${process.env.HOST}:${process.env.PORT} - ${process.env.ENV}`);
